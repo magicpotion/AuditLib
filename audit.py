475fbec194a84c57349f6e86b79a47a3fdd77dbf
chris <chris@debian-chris>
2016-01-11 17:19:19 +0000

    some new modifications

--- audit.py
+++ audit.py
@@ -20,43 +20,47 @@ from kombu import BrokerConnection, Exchange
 
 
 class ExchangeSetup:
-    def __init__(self, exchange='logs', exchange_type='direct',
-                 routing_key='log_messages'):
+    def __init__(self, exchange='audit', exchange_type='direct',
+                 routing_key='log.messages', queue='logs'):
         """Broker exchange.
 
         Args:
             exchange (str): Exchange name
             exchange_type (str): AMQP exchange type, see your broker manual
             routing_key (str)
+            queue (str)
         """
         self.exchange = exchange
         self.exchange_type = exchange_type
         self.routing_key = routing_key
+        self.queue = queue
 
 
 class Audit:
     def __init__(self, hostname='localhost', port='5672',
-                 userid='', password='', virtual_host='/',
+                 userid='', password='', virtual_host='graylog',
                  exchange=None):
         self.hostname = hostname
         self.port = port
         self.userid = userid
         self.password = password
         self.virtual_host = virtual_host
-        self.connection = BrokerConnection()
+        self.connection = BrokerConnection(virtual_host=virtual_host)
         self.exchange_setup = exchange or ExchangeSetup()
 
-    def custom_exchange(self, exchange, exchange_type, routing_key):
+    def custom_exchange(self, exchange, exchange_type, routing_key, queue):
         """Broker exchange can be set after the object has been instantiated.
 
         Args:
             exchange (str): Exchange name
             exchange_type (str): AMQP exchange type, see your broker manual
             routing_key (str)
+            queue (str)
         """
         self.exchange_setup.exchange = exchange
         self.exchange_setup.exchange_type = exchange_type
         self.exchange_setup.routing_key = routing_key
+        self.exchange_setup.queue = queue
 
     def log(self, message):
         """Pushes argument object to message broker.
@@ -70,12 +74,12 @@ class Audit:
         channel = self.connection.channel()
         exchange = Exchange(self.exchange_setup.exchange,
                             type=self.exchange_setup.exchange_type)
-        # bound queue - it is a standard to always declare it
-        queue = exchange(channel)
-        queue.declare(nowait=True)
+
+        bound_exchange = exchange(channel)
+        bound_exchange.declare()
 
         # example_message = '{"short_message":"Kombu", "host":"example.org"}'
-        message = queue.Message(message)
-        queue.publish(message, routing_key=self.exchange_setup.routing_key)
+        message = bound_exchange.Message(message)
+        bound_exchange.publish(message, routing_key=self.exchange_setup.routing_key)
 
         self.connection.release()
